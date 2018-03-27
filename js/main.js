77b534392358c0fb2c53b69bccaaa630d981fd88
aawadall <aawadall@ualberta.ca>
2018-03-26 20:55:26 -0400

    now getting repo details

--- js/main.js
+++ js/main.js
@@ -5,10 +5,17 @@
 
         var onUserComplete = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function (response){
+            
+            $scope.repos = response.data;
         };
 
         var onError = function (reason) {
-            $scope.error = "Could not fetch user";
+            $scope.error = "Could not fetch data";
         };
 
         $scope.search  = function(username){
